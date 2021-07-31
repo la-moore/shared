a1ca274abb63213027c8fa4c33a2af2eb5ba067e
moorlex <moorlex@mail.com>
2021-07-31 17:33:35 +0300

    core: update

--- plugins/icons/index.ts
+++ plugins/icons/index.ts
@@ -1,14 +1,13 @@
 import { Plugin } from 'vue'
 import { useIcons } from './state'
-import baseIcons from './icons'
+import baseIcons from './base'
 import { useComponent } from '/-/components'
 import Icon from './components/icon.vue'
 
 const plugin: Plugin = function(app, icons?: any) {
   const { addIcons } = useIcons()
 
-  addIcons(baseIcons)
-  addIcons(icons)
+  addIcons(icons || baseIcons)
 
   useComponent(app, null, 'Icon', Icon)
 }
--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -4,29 +4,50 @@ const path = require('path')
 const ICONS_DIR = path.join(__dirname, '../icons/')
 const OUTPUT_DIR = path.join(__dirname, '../plugins/icons/')
 
-const files = [];
+const files = {};
 
 function ThroughDirectory(directory) {
   fs.readdirSync(directory).forEach(File => {
     const absolute = path.join(directory, File)
-    if (fs.statSync(absolute).isDirectory()) return ThroughDirectory(absolute)
-    else return files.push(absolute.replace(ICONS_DIR, '').split('.').shift())
+    const fullPath = absolute
+      .replace(ICONS_DIR, '')
+      .replace(/(\\)/g, '/')
+      .split('.')
+      .shift()
+
+    if (fs.statSync(absolute).isDirectory()) {
+      files[fullPath] = []
+      return ThroughDirectory(absolute)
+    } else {
+      const basePath = fullPath.split('/')[0]
+      const currentPath = fullPath.split('/')
+        .slice(1)
+        .join('/')
+
+      return files[basePath].push(currentPath)
+    }
   })
 }
 
 ThroughDirectory(ICONS_DIR)
 
-const arr = {}
+Object.keys(files).forEach((base) => {
+  const arr = {}
 
-files.forEach((file) => {
-  let fileContent = fs.readFileSync(`${ICONS_DIR}/${file}.svg`, 'utf8')
-  let name = file.replace(/(\\)/g, '/')
+  if (files[base].length === 0) {
+    return
+  }
 
-  arr[name] = fileContent
+  files[base].forEach((file) => {
+    let fileContent = fs.readFileSync(`${ICONS_DIR}/${base}/${file}.svg`, 'utf8')
+    let name = file.replace(/(\\)/g, '/')
+
+    arr[name] = fileContent
       .replace((/"/gm), "'")
-})
+  })
 
-const out = 'export default ' + JSON.stringify(arr)
+  const out = 'export default ' + JSON.stringify(arr)
     .replace((/\s\s|\t|\\r\\n|\\n|\\r/gm),"")
 
-fs.writeFileSync(path.join(OUTPUT_DIR, `icons.ts`), out.replace((/","/gm),"\",\n\t\"").replace((/"}/gm),"\"\n}").replace((/{"/gm),"{\n\t\"").replace((/":"/gm),"\": \""))
+  fs.writeFileSync(path.join(OUTPUT_DIR, `${base}.ts`), out.replace((/","/gm),"\",\n\t\"").replace((/"}/gm),"\"\n}").replace((/{"/gm),"{\n\t\"").replace((/":"/gm),"\": \""))
+})
