4134932edeaf966066a54377480c298274683682
moorlex <moorlex@mail.com>
2021-11-30 13:40:15 +0300

    core: fix

--- helpers/serialization.ts
+++ helpers/serialization.ts
@@ -1,10 +1,10 @@
-import { ClassConstructor, classToPlain, plainToClass } from 'class-transformer'
+import { ClassConstructor, instanceToPlain, plainToInstance } from 'class-transformer'
 
 export function deserialize(obj: any, cls: ClassConstructor<any>) {
-  return plainToClass(cls, obj, { /* params */ })
+  return plainToInstance(cls, obj, { /* params */ })
 }
 
 export function serialize(obj: any, cls?: ClassConstructor<any>) {
   if (cls) { Object.setPrototypeOf(obj, cls.prototype) }
-  return classToPlain(obj, { exposeDefaultValues: true })
+  return instanceToPlain(obj, { exposeDefaultValues: true })
 }
