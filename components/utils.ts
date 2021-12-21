12c8df26b34f0773a309e78b62247448e61634cf
moorlex <moorlex@mail.com>
2021-12-21 12:09:19 +0300

    core: update rounding utils

--- components/utils.ts
+++ components/utils.ts
@@ -3,6 +3,9 @@ export const ROUNDED = {
   sm: 'rounded-sm',
   md: 'rounded-md',
   lg: 'rounded-lg',
+  xl: 'rounded-xl',
+  '2xl': 'rounded-2xl',
+  '3xl': 'rounded-3xl',
   full: 'rounded-full',
 }
 
